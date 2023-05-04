1a2a4bf8b4d3475a70f6cf10e305b1bb01514da1
Jayaram Karunakaran <126938644+jayaram-karunakaran@users.noreply.github.com>
2023-05-04 07:47:47 +0530

    Added files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-5 bg-sky-300 font-bold text-3xl">
+      React with Tailwind CSS and Typescript Template
+    </div>
+  );
+}
+
+export default App;
